4dc7bf9036551147d3aac46c88b27d44f4743059
Ervin Louie Ong <ervin_louie_ong@yahoo.com>
2017-06-09 22:15:21 +0000

    Add has_many :reviews and validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
 
-  validates :rating, numericality: true
+  validates :rating, numericality: { greater_than: 0, less_than: 6 }, presence: true
 end
